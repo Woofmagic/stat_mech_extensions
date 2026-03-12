0bcf1b916e2d14976501c2affd21941bfaebc0e8
ErenMcLaren <dima.watkins@gmail.com>
2026-03-12 14:22:12 -0400

    updated and modernized the state space orbit plot

--- lv_lagrangian/standard_lv_phase_plot_v1.py
+++ lv_lagrangian/standard_lv_phase_plot_v1.py
@@ -2,6 +2,9 @@
 The script that was used to generate (i) plots showing the kinematics associated with the 
 derived second-order ODE that represents the single "LV particle" and (ii) plots showing the
 "potential well" that we have interpreted the LV particle to be moving within.
+
+Date initialized: 20260312
+Last edited: 20260312
 """
 
 # 3rd Party Library | NumPy
@@ -19,102 +22,55 @@ from matplotlib.colors import Normalize
 # 3rd Party Library | SciPy:
 from scipy.integrate import solve_ivp
 
-# (X): Define a version number so we don't get confused:
-_version_number = "1.1"
+#################################
+# File Information
+#################################
 
-# (X): Dynamically set the plot title using the version number:
+_version_number = "1.2"
 PLOT_TITLE = f"lv_phase_dynamics_generic_params_v{_version_number}"
 
-# (X): Fix the plot directory for the SIS analysis:
-# PLOT_DIRECTORY = "plots"
-
-# (X): We tell rcParams to use LaTeX. Note: this will *crash* your 
+#################################
+# Plotting Configuration
+#################################
+# (X): We tell rcParams to use LaTeX. [NOTE]: this will *crash* your 
 # | version of the code if you do not have TeX distribution installed!
-plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "serif",
-})
-
-# (X): rcParams for the x-axis tick direction:
+plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
 plt.rcParams['xtick.direction'] = 'in'
-
-# (X): rcParams for the "major" (larger) x-axis vertical size:
 plt.rcParams['xtick.major.size'] = 5
-
-# (X): rcParams for the "major" (larger) x-axis horizonal width:
 plt.rcParams['xtick.major.width'] = 0.5
-
-# (X): rcParams for the "minor" (smaller) x-axis vertical size:
 plt.rcParams['xtick.minor.size'] = 2.5
-
-# (X): rcParams for the "minor" (smaller) x-axis horizonal width:
 plt.rcParams['xtick.minor.width'] = 0.5
-
-# (X): rcParams for the minor ticks to be *shown* versus invisible:
 plt.rcParams['xtick.minor.visible'] = True
-
-# (X): rcParams dictating that we want ticks along the x-axis on *top* (opposite side) of the bounding box:
 plt.rcParams['xtick.top'] = True    
-
-# (X): rcParams for the y-axis tick direction:
 plt.rcParams['ytick.direction'] = 'in'
-
-# (X): rcParams for the "major" (larger) y-axis vertical size:
 plt.rcParams['ytick.major.size'] = 5
-
-# (X): rcParams for the "major" (larger) y-axis horizonal width:
 plt.rcParams['ytick.major.width'] = 0.5
-
-# (X): rcParams for the "minor" (smaller) y-axis vertical size:
 plt.rcParams['ytick.minor.size'] = 2.5
-
-# (X): rcParams for the "minor" (smaller) y-axis horizonal width:
 plt.rcParams['ytick.minor.width'] = 0.5
-
-# (X): rcParams for the minor ticks to be *shown* versus invisible:
 plt.rcParams['ytick.minor.visible'] = True
-
-# (X): rcParams dictating that we want ticks along the y-axis on the *left* of the bounding box:
 plt.rcParams['ytick.right'] = True
 
-# (X): Simulation Parameter | prey exponential increase:
-PARAMETER_ALPHA = 0.66666
+#################################
+# Simulation Parameters
+#################################
+PARAMETER_ALPHA = 0.66666 # prey exponential increase
+PARAMETER_BETA = 1.33333 # predator hunting "power
+PARAMETER_GAMMA = 1.0 # predator exponential decrease
+PARAMETER_DELTA = 1.0 # prey presence measurement:
 
-# (X): Simulation Parameter | predator hunting "power":
-PARAMETER_BETA = 1.33333
+INITIAL_PREY = 1.0
+INITIAL_PREDATORS = 0.08
 
-# (X): Simulation Parameter | predator exponential decrease:
-PARAMETER_GAMMA = 1.0
+TIME_STARTING_VALUE = 0 #simulation's starting "t" value
+TIME_ENDING_VALUE = 10 #simulation's ending "t" value
+NUMBER_OF_TIME_SLICES = 1000 #simulation's Δt
 
-# (X): Simulation Parameter | prey presence measurement:
-PARAMETER_DELTA = 1.0
-
-# (X): Simulation Parameter | predator's "initial position"
-# | This corresponds to the predator's initial population density
-# | in the original model.
-INITIAL_POSITION = 1.0
-
-# (X): Simulation Parameter | predator's "initial velocity"
-# | This corresponds the 
-INITIAL_VELOCITY = 0.08
-
-# (X): Simulation Parameter | simulation's starting "t" value:
-TIME_STARTING_VALUE = 0
-
-# (X): Simulation Parameter | simulation's ending "t" value:
-TIME_ENDING_VALUE = 100
-
-# (X): Simulation Parameter | simulation's Δt:
-NUMBER_OF_TIME_SLICES = 1000
-
-# (X): Construct a tuple to use later in NumPy methods that 
-# | represents the time interval over which the simulation will
-# | take place.
 TIME_INTERVAL = (TIME_STARTING_VALUE, TIME_ENDING_VALUE)
-
-# (X): Unpack the tuple immediately and obtain an array of numerical
-# | time-slices that we will plug-and-chug into the numerical ODE solver.
 TIME_SLICES = np.linspace(*TIME_INTERVAL, NUMBER_OF_TIME_SLICES)
+
+#################################
+# Functions:
+#################################
 
 def second_order_ode(t, vector_of_equations):
     """
@@ -129,57 +85,32 @@ def second_order_ode(t, vector_of_equations):
     (i) x' = v, (ii) v' = f(x), where (iii) x'' = f(x), the original ODE.
     (Primes are time-derivatives, of course.)
     """
-    # (1): Unpack the standard "vector" of dynamical variables: x^[T] rep. as (x, v):
     x, y = vector_of_equations
-
-    # (2): As per reduction of order, x' is nothing but v:
     x_dot = PARAMETER_ALPHA * x - PARAMETER_BETA * x * y
-
-    # (3): As per reduction of order, v' is nothing but x'', and it
-    # | *is* the standard ODE:
     y_dot = -PARAMETER_GAMMA * y + PARAMETER_DELTA * x * y
-    
-    # (4): Return an "effective vector" (list) that is just dot(x)^{T} = (x', v'):
     return [x_dot, y_dot]
 
 # (X): Use SciPy's IVP numerical integrator to do the calculation.
 # | This is *the* actual simulation. This is where it happens:
 numerical_solution = solve_ivp(
-
-    # (X.1): It needs *the* function we are numerically integrating:
     fun = second_order_ode, 
-
-    # (X.2): It needs the interval of the real line (time axis) to do the integration over:
     t_span = TIME_INTERVAL, 
-
-    # (X.3): It needs the initial conditions as a *vector* (list):
-    y0 = [INITIAL_POSITION, INITIAL_VELOCITY],
-
-    # (X.4): Needs the total number of time-steps to numerically evaluate the system:
+    y0 = [INITIAL_PREY, INITIAL_PREDATORS],
     t_eval = TIME_SLICES)
 
-# (X): As part of SciPy's `solve_ivp`, extract the time "history":
 time_axis = numerical_solution.t
-
-# (X): Same as above, but extract the first component of the y *vector* (list):
 prey_density = numerical_solution.y[0]
-
-# (X): Same as above, but now the second component:
 predator_density = numerical_solution.y[1]
-
-# (X): We solved for x and v, and *now* we can plug-and-chug for the corresponding x'':
 a = PARAMETER_ALPHA * PARAMETER_GAMMA + (PARAMETER_ALPHA - PARAMETER_BETA * np.exp(prey_density)) * predator_density - PARAMETER_BETA * PARAMETER_GAMMA * np.exp(prey_density)
 
-# (1): Set up the Figure instance and the *two* Axes objects:
+#################################
+# Plotting flow:
+#################################
+
 figure_instance, axis_instance =  plt.subplots(
-    nrows = 1, 
-    ncols = 1, 
-    sharex = True,
-    figsize = (10, 5.5))
+    nrows = 1, ncols = 1, figsize = (10, 5.5))
 
-# (X): The rest of the script just makes the plots. We will comment them better later:
 points = np.array([prey_density, predator_density]).T.reshape(-1, 1, 2)
-
 segments = np.concatenate([points[:-1], points[1:]], axis = 1)
 
 norm = Normalize(time_axis.min(), time_axis.max())
@@ -188,26 +119,20 @@ lc.set_array(time_axis)
 lc.set_linewidth(2)
 line = axis_instance.add_collection(lc)
 
-axis_instance.set_xlabel(r"Prey Density $x$")
-axis_instance.set_ylabel(r"Predator Density $y$")
-axis_instance.set_title(fr"LV Phase Evolution with with $\alpha = {PARAMETER_ALPHA}, \beta = {PARAMETER_BETA}, \gamma = {PARAMETER_GAMMA}, \delta = {PARAMETER_DELTA}$", fontsize = 18)
-axis_instance.set_xlim(prey_density.min(), prey_density.max())
-axis_instance.set_ylim(predator_density.min(), predator_density.max())
+axis_instance.set_xlabel(r"Prey Density $x$", rotation = 0, labelpad = 5.0, fontsize = 18)
+axis_instance.set_ylabel(r"Predator Density $y$", rotation = 90, labelpad = 17.0, fontsize = 18)
+axis_instance.set_title(
+    fr"LV State Space Evolution with $\alpha = {PARAMETER_ALPHA}, \beta = {PARAMETER_BETA}, \gamma = {PARAMETER_GAMMA}, \delta = {PARAMETER_DELTA}$", 
+    fontsize = 18)
+
+axis_instance.set_xlim(prey_density.min() - 0.5, prey_density.max() + 0.5)
+axis_instance.set_ylim(predator_density.min() - 0.5, predator_density.max() + 0.5)
 axis_instance.tick_params(labelsize = 17)
 
-# (X): Add a grid for clarity:
-plt.grid(True)
+axis_instance.grid(True)
 
-# (X): Tight layout... ya know:
-plt.tight_layout(pad = 2.0)
-
-# (X): Save a version of the figure according to .eps format for Overleaf stuff:
-# plt.savefig(f"{PLOT_DIRECTORY}/{PLOT_TITLE}.eps", format = "eps")
 plt.savefig(f"{PLOT_TITLE}.eps", format = "eps")
-
-# (X): Save an immediately-visualizable figure with vector graphics:
-# plt.savefig(f"{PLOT_DIRECTORY}/{PLOT_TITLE}.svg", format = "svg")
 plt.savefig(f"{PLOT_TITLE}.svg", format = "svg")
+plt.close(figure_instance)
 
-# (X): Closing figures:
-plt.close()
+print("[INFO]: End of script reached!")
