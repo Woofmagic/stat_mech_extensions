40c20747c76fe2691cd358e57bd0b549722ffc94
MidnightExcursion <dima.watkins@gmail.com>
2026-07-03 17:58:36 -0400

    added the simulation and plotting logic that makes heat capacity figures

--- lv_stat_mech/lv_stat_mech_plots.py
+++ lv_stat_mech/lv_stat_mech_plots.py
@@ -1,8 +1,16 @@
 """
-The script that was used to visualize the "potential well" that we interpret the 
-"predator particle" to move within. In classical physics, the potential well picture
-is quite informative, so it helps to see (if you can!) what the particle is moving "in."
+A large(r) script that generates a bunch of plots that depict the LV statistical quantities as 
+functions of temperature or inverse temperature.
 """
+
+print("[INFO]: Script started!")
+
+####################################################################################################
+# Loading libraries:
+####################################################################################################
+
+# Native library | Path
+from pathlib import Path 
 
 # 3rd Party Library | NumPy
 import numpy as np
@@ -12,169 +20,266 @@ from scipy.special import polygamma
 
 # 3rd Party Library | Matplotlib:
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-# (X): Define a version number so we don't get confused:
-_version_number = "3.1"
+####################################################################################################
+# Versioning:
+####################################################################################################
 
-# (X): Fix the plot directory for the LV analysis:
-# PLOT_DIRECTORY = "plots"
+_VERSION_NUMBER = "3_2"
 
-# (X): We tell rcParams to use LaTeX. Note: this will *crash* your 
-# | version of the code if you do not have TeX distribution installed!
-plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "serif",
-})
+####################################################################################################
+# Plotting customization :
+# [NOTE]: this will *crash* your version of the code if you do not have TeX distribution installed!
+####################################################################################################
 
-# (X): rcParams for the x-axis tick direction:
+plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
 plt.rcParams['xtick.direction'] = 'in'
-
-# (X): rcParams for the "major" (larger) x-axis vertical size:
-plt.rcParams['xtick.major.size'] = 5
-
-# (X): rcParams for the "major" (larger) x-axis horizonal width:
+plt.rcParams['xtick.major.size'] = 8.5
 plt.rcParams['xtick.major.width'] = 0.5
-
-# (X): rcParams for the "minor" (smaller) x-axis vertical size:
-plt.rcParams['xtick.minor.size'] = 2.5
-
-# (X): rcParams for the "minor" (smaller) x-axis horizonal width:
+plt.rcParams['xtick.minor.size'] = 3.5
 plt.rcParams['xtick.minor.width'] = 0.5
-
-# (X): rcParams for the minor ticks to be *shown* versus invisible:
 plt.rcParams['xtick.minor.visible'] = True
-
-# (X): rcParams dictating that we want ticks along the x-axis on *top* (opposite side) of the bounding box:
-plt.rcParams['xtick.top'] = True    
-
-# (X): rcParams for the y-axis tick direction:
+plt.rcParams['xtick.top'] = True
+plt.rcParams['xtick.labelsize'] = 15
 plt.rcParams['ytick.direction'] = 'in'
-
-# (X): rcParams for the "major" (larger) y-axis vertical size:
-plt.rcParams['ytick.major.size'] = 5
-
-# (X): rcParams for the "major" (larger) y-axis horizonal width:
+plt.rcParams['ytick.major.size'] = 8.5
 plt.rcParams['ytick.major.width'] = 0.5
-
-# (X): rcParams for the "minor" (smaller) y-axis vertical size:
-plt.rcParams['ytick.minor.size'] = 2.5
-
-# (X): rcParams for the "minor" (smaller) y-axis horizonal width:
+plt.rcParams['ytick.minor.size'] = 3.5
 plt.rcParams['ytick.minor.width'] = 0.5
-
-# (X): rcParams for the minor ticks to be *shown* versus invisible:
 plt.rcParams['ytick.minor.visible'] = True
-
-# (X): rcParams dictating that we want ticks along the y-axis on the *left* of the bounding box:
 plt.rcParams['ytick.right'] = True
+plt.rcParams['ytick.labelsize'] = 14
+plt.rcParams['savefig.dpi'] = 300
 
-# (X): Simulation Parameter | prey exponential increase:
+####################################################################################################
+# Pathing:
+####################################################################################################
+
+# (X): Define the base path:
+PLOT_DIRECTORY = Path('./')
+
+####################################################################################################
+# PLOT: Required numerical ingredients, including simulations parameters and temperature ranges
+####################################################################################################
+
 PARAMETER_ALPHA = 0.66666
-
-# (X): Simulation Parameter | predator hunting "power":
 PARAMETER_BETA = 1.33333
-
-# (X): Simulation Parameter | predator exponential decrease:
 PARAMETER_GAMMA = 1.0
-
-# (X): Simulation Parameter | prey presence measurement:
 PARAMETER_DELTA = 1.0
 
-inverse_temperature = np.linspace(start = 3.0, stop = 0.1, num = 10000)
 temperature = np.linspace(start = 0.01, stop = 5.0, num = 10000)
+inverse_temperature = 1.0 / temperature
+
+####################################################################################################
+# PLOT: Average Energy vs. Inverse Temperature
+####################################################################################################
 
 # (X): We derived this analytically:
-average_energy_vs_theta = (
-    PARAMETER_ALPHA +
-    PARAMETER_GAMMA +
-    PARAMETER_ALPHA * np.log(PARAMETER_BETA * inverse_temperature) +
-    PARAMETER_GAMMA * np.log(PARAMETER_DELTA * inverse_temperature) -
-    PARAMETER_ALPHA * polygamma(0, PARAMETER_ALPHA * inverse_temperature) -
-    PARAMETER_GAMMA * polygamma(0, PARAMETER_GAMMA * inverse_temperature)
-    )
+def lv_average_energy(temperature, alpha, beta, gamma, delta):
+    return (
+        alpha +
+        gamma +
+        alpha * np.log(beta * temperature) +
+        gamma * np.log(delta * temperature) -
+        alpha * polygamma(0, alpha * temperature) -
+        gamma * polygamma(0, gamma * temperature)
+        )
 
-average_energy_vs_temperature = (
-    PARAMETER_ALPHA +
-    PARAMETER_GAMMA +
-    PARAMETER_ALPHA * np.log(PARAMETER_BETA * (1./ temperature)) +
-    PARAMETER_GAMMA * np.log(PARAMETER_DELTA * (1./ temperature)) -
-    PARAMETER_ALPHA * polygamma(0, PARAMETER_ALPHA * (1./ temperature)) -
-    PARAMETER_GAMMA * polygamma(0, PARAMETER_GAMMA * (1./ temperature))
-    )
+def lv_specific_heat(temperature, alpha, gamma):
+    return ((
+        -alpha
+        - gamma
+        + alpha**2 * polygamma(1, alpha / temperature) / temperature
+        + gamma**2 * polygamma(1, gamma / temperature) / temperature
+    ) / temperature)
 
-###################################
-# PLOT: Average Energy vs. Theta
-###################################
-energy_vs_theta_figure = plt.figure(figsize = (10, 5.5))
-energy_vs_theta_axis = energy_vs_theta_figure.add_subplot(1, 1, 1)
+####################################################################################################
+# PLOT: Average Energy vs. Inverse Temperature
+####################################################################################################
+energy_vs_sigma_figure = plt.figure(figsize = (10, 5.5))
+energy_vs_sigma_axis = energy_vs_sigma_figure.add_subplot(1, 1, 1)
 
-energy_vs_theta_axis.hlines(
+energy_vs_sigma_axis.hlines(
     y = 0., xmin = 0.0 + 0.0 * np.min(inverse_temperature), xmax = np.max(inverse_temperature),
     colors = "black", linestyles = '--', alpha = 0.5,
-    label = r"$\bar{E} = 0$")
+    label = r"$\langle E \rangle = 0$")
 
-energy_vs_theta_axis.plot(
-    inverse_temperature, average_energy_vs_theta,
+energy_vs_sigma_axis.plot(
+    inverse_temperature, lv_average_energy(inverse_temperature, PARAMETER_ALPHA, PARAMETER_BETA, PARAMETER_GAMMA, PARAMETER_DELTA),
     color = "black", linestyle = '-', linewidth = 1.5,
     label = r"Average Energy Trend")
 
-energy_vs_theta_axis.set_title(
-    fr"$\bar{{E}}$ vs. Inverse Temperature for $\alpha = {PARAMETER_ALPHA}, \beta = {PARAMETER_BETA}, \gamma = {PARAMETER_GAMMA}, \delta = {PARAMETER_DELTA}$",
+energy_vs_sigma_axis.set_title(
+    fr"$\langle E \rangle$ vs. Inverse Temperature for $\alpha = {PARAMETER_ALPHA}, \beta = {PARAMETER_BETA}, \gamma = {PARAMETER_GAMMA}, \delta = {PARAMETER_DELTA}$",
     fontsize = 18
 )
 
-energy_vs_theta_axis.set_xlabel(xlabel = r"$\theta$ (Inverse Temperature)", fontsize = 17, labelpad = 4.,)
-energy_vs_theta_axis.set_ylabel(ylabel = r"$\bar{E} \left( \theta \right)$", fontsize = 17, labelpad = 25.0, rotation = 0)
-energy_vs_theta_axis.tick_params(labelsize = 17)
-energy_vs_theta_axis.legend(fontsize = 18)
+energy_vs_sigma_axis.set_xlabel(xlabel = r"$\sigma$ (Inverse Temperature)", fontsize = 17, labelpad = 4.,)
+energy_vs_sigma_axis.set_ylabel(ylabel = r"$\langle E \rangle \left( \sigma \right)$", fontsize = 17, labelpad = 25.0, rotation = 0)
+energy_vs_sigma_axis.tick_params(labelsize = 17)
+energy_vs_sigma_axis.legend(fontsize = 18)
 
-# (X): Save a version of the figure according to .eps format for Overleaf stuff:
-# plt.savefig(f"{PLOT_DIRECTORY}/{PLOT_TITLE}.eps", format = "eps")
-energy_vs_theta_figure.savefig(f"lv_avg_energy_inv_temp_{_version_number}.eps", format = "eps")
-
-# (X): Save an immediately-visualizable figure with vector graphics:
-# plt.savefig(f"{PLOT_DIRECTORY}/{PLOT_TITLE}.svg", format = "svg")
-energy_vs_theta_figure.savefig(f"lv_avg_energy_inv_temp_{_version_number}.svg", format = "svg")
+for extension in ["eps", "svg", "png"]:
+    energy_vs_sigma_figure.savefig(
+        PLOT_DIRECTORY /
+        "plots" /
+        f"lv_avg_energy_inv_temp_{_VERSION_NUMBER}.{extension}", 
+        format = extension)
 
 # (X): Closing figures:
-plt.close(energy_vs_theta_figure)
+plt.close(energy_vs_sigma_figure)
 
-###################################
+####################################################################################################
 # PLOT: Average Energy vs. Temperature
-###################################
+####################################################################################################
 energy_vs_temp_figure = plt.figure(figsize = (10, 5.5))
 energy_vs_emp_axis = energy_vs_temp_figure.add_subplot(1, 1, 1)
 
 energy_vs_emp_axis.hlines(
     y = 0., xmin = 0.0 + 0.0 * np.min(temperature), xmax = np.max(temperature),
     colors = "black", linestyles = '--', alpha = 0.5,
-    label = r"$\bar{E} = 0$")
+    label = r"$\langle E \rangle = 0$")
 
 energy_vs_emp_axis.plot(
-    temperature, average_energy_vs_temperature,
+    temperature, lv_average_energy(temperature, PARAMETER_ALPHA, PARAMETER_BETA, PARAMETER_GAMMA, PARAMETER_DELTA),
     color = "black", linestyle = '-', linewidth = 1.5,
     label = r"Average Energy Trend")
 
 energy_vs_emp_axis.set_title(
-    fr"$\bar{{E}}$ vs. Temperature for $\alpha = {PARAMETER_ALPHA}, \beta = {PARAMETER_BETA}, \gamma = {PARAMETER_GAMMA}, \delta = {PARAMETER_DELTA}$",
+    fr"$\langle E \rangle$ vs. Temperature for $\alpha = {PARAMETER_ALPHA}, \beta = {PARAMETER_BETA}, \gamma = {PARAMETER_GAMMA}, \delta = {PARAMETER_DELTA}$",
     fontsize = 18
 )
 
 energy_vs_emp_axis.set_xlabel(
     xlabel = r"$T$ (Temperature, ``$k_{\textrm{B}}$''$ = 1$)", fontsize = 17, labelpad = 4.,)
 energy_vs_emp_axis.set_ylabel(
-    ylabel = r"$\bar{E} \left( T \right)$", fontsize = 17, labelpad = 25.0, rotation = 0)
+    ylabel = r"$\langle E \rangle \left( T \right)$", fontsize = 17, labelpad = 25.0, rotation = 0)
 
 energy_vs_emp_axis.tick_params(labelsize = 17)
 energy_vs_emp_axis.legend(fontsize = 18)
 
-# (X): Save a version of the figure according to .eps format for Overleaf stuff:
-# plt.savefig(f"{PLOT_DIRECTORY}/{PLOT_TITLE}.eps", format = "eps")
-energy_vs_temp_figure.savefig(f"lv_avg_energy_temp_{_version_number}.eps", format = "eps")
-
-# (X): Save an immediately-visualizable figure with vector graphics:
-# plt.savefig(f"{PLOT_DIRECTORY}/{PLOT_TITLE}.svg", format = "svg")
-energy_vs_temp_figure.savefig(f"lv_avg_energy_temp_{_version_number}.svg", format = "svg")
+for extension in ["eps", "svg", "png"]:
+    energy_vs_temp_figure.savefig(
+        PLOT_DIRECTORY /
+        "plots" /
+        f"lv_avg_energy_temp_{_VERSION_NUMBER}.{extension}", 
+        format = extension)
 
 # (X): Closing figures:
-plt.close(energy_vs_theta_figure)
+plt.close(energy_vs_sigma_figure)
+
+####################################################################################################
+# PLOT: Heat Capacity vs. Temperature:
+# [NOTE]: this plot is a little bit more involved than the others:
+####################################################################################################
+
+# (X): make the range of temperature values:
+temperature = np.linspace(1e-6, 11.0, 1000)
+# (X): make the range of parameter values:
+alpha_values = np.linspace(1e-4, 5.0, 50)
+gamma_values = np.linspace(1e-4, 5.0, 50)
+
+# (X): fix the parameters:
+FIXED_GAMMA = 2.0
+FIXED_ALPHA = 2.0
+
+# (X): do some matplotlib colormapping business:
+colormap = plt.get_cmap("magma")
+normalized_colors = mpl.colors.Normalize(vmin = alpha_values.min(), vmax = alpha_values.max())
+
+# (X): Create a ScalarMappable for the colorbar.
+scalar_mappable = plt.cm.ScalarMappable(cmap = colormap, norm = normalized_colors)
+scalar_mappable.set_array([])
+
+# (X): Now, we actually start plotting things:
+
+fixed_gamma_heat_capacity_figure = plt.figure(figsize = (10, 6))
+fixed_gamma_heat_capacity_axis = fixed_gamma_heat_capacity_figure.add_subplot(1, 1, 1)
+
+for alpha in alpha_values:
+
+    fixed_gamma_heat_capacity_axis.plot(
+        temperature, lv_specific_heat(temperature, alpha, FIXED_GAMMA),
+        color = colormap(normalized_colors(alpha)), linewidth = 1.0, alpha = 0.8,
+    )
+
+# (X): Need a colorbar to indicate how the parameters vary:
+fixed_gamma_colorbar = fixed_gamma_heat_capacity_figure.colorbar(
+    scalar_mappable, ax = fixed_gamma_heat_capacity_axis, pad = 0.02)
+
+fixed_gamma_colorbar.set_label(r"Varying parameter $\alpha$", fontsize = 16.0)
+fixed_gamma_colorbar.ax.tick_params(labelsize = 14.0)
+
+fixed_gamma_heat_capacity_axis.hlines(
+    y = 1.0, xmin = 0.0, xmax = 10.0,
+    linestyles = "--", color = "gray", label = r"$C_V(T) = 1$")
+fixed_gamma_heat_capacity_axis.hlines(
+    y = 2.0, xmin = 0.0, xmax = 10.0,
+    linestyles = "--", color = "red", label = r"$C_V(T) = 2$")
+fixed_gamma_heat_capacity_axis.set_xlim(-0.5, 10.5)
+fixed_gamma_heat_capacity_axis.set_ylim(0.0, 2.25)
+
+fixed_gamma_heat_capacity_axis.set_title(
+    fr"$C_V(T)$ Trend for Varying $\alpha$ with Fixed $\gamma = {FIXED_GAMMA}$", fontsize = 18.0)
+fixed_gamma_heat_capacity_axis.set_xlabel(
+    r"$T$", fontsize = 17.0)
+fixed_gamma_heat_capacity_axis.set_ylabel(
+    r"$C_V(T)$", fontsize = 17.0)
+fixed_gamma_heat_capacity_axis.legend(fontsize = 14.0, loc = "lower right")
+
+fixed_gamma_heat_capacity_figure.tight_layout()
+
+for extension in ["eps", "svg", "png"]:
+    fixed_gamma_heat_capacity_figure.savefig(
+        PLOT_DIRECTORY /
+        "plots" /
+        f"lv_hc_vary_alpha_{_VERSION_NUMBER}.{extension}", format = extension)
+
+plt.close(fixed_gamma_heat_capacity_figure)
+
+fixed_alpha_heat_capacity_figure = plt.figure(figsize = (10, 6))
+fixed_alpha_heat_capacity_axis = fixed_alpha_heat_capacity_figure.add_subplot(1, 1, 1)
+
+for gamma_value in gamma_values:
+
+    fixed_alpha_heat_capacity_axis.plot(
+        temperature, lv_specific_heat(temperature, FIXED_ALPHA, gamma_value),
+        color = colormap(normalized_colors(gamma_value)), linewidth = 1.0, alpha = 0.8,
+    )
+
+# (X): Need a colorbar to indicate how the parameters vary:
+fixed_alpha_colorbar = fixed_alpha_heat_capacity_figure.colorbar(
+    scalar_mappable, ax = fixed_alpha_heat_capacity_axis, pad = 0.02)
+
+fixed_alpha_colorbar.set_label(r"Varying parameter $\gamma$", fontsize = 16.0)
+fixed_alpha_colorbar.ax.tick_params(labelsize = 14.0)
+
+fixed_alpha_heat_capacity_axis.hlines(
+    y = 1.0, xmin = 0.0, xmax = 10.0,
+    linestyles = "--", color = "gray", label = r"$C_V(T) = 1$")
+fixed_alpha_heat_capacity_axis.hlines(
+    y = 2.0, xmin = 0.0, xmax = 10.0,
+    linestyles = "--", color = "red", label = r"$C_V(T) = 2$")
+fixed_alpha_heat_capacity_axis.set_xlim(-0.5, 10.5)
+fixed_alpha_heat_capacity_axis.set_ylim(0.0, 2.25)
+
+fixed_alpha_heat_capacity_axis.set_title(
+    fr"$C_V(T)$ Trend for Varying $\gamma$ with Fixed $\alpha = {FIXED_ALPHA}$", fontsize = 18.0)
+fixed_alpha_heat_capacity_axis.set_xlabel(
+    r"$T$", fontsize = 17.0)
+fixed_alpha_heat_capacity_axis.set_ylabel(
+    r"$C_V(T)$", fontsize = 17.0)
+fixed_alpha_heat_capacity_axis.legend(fontsize = 14.0, loc = "lower right")
+
+fixed_gamma_heat_capacity_figure.tight_layout()
+
+for extension in ["eps", "svg", "png"]:
+    fixed_alpha_heat_capacity_figure.savefig(
+        PLOT_DIRECTORY /
+        "plots" /
+        f"lv_hc_vary_gamma_{_VERSION_NUMBER}.{extension}", format = extension)
+
+plt.close(fixed_alpha_heat_capacity_figure)
+
+print("[INFO]: Script finished running!")
